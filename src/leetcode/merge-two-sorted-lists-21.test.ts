283adbba77ee25f2eb2bef87280f907a1457de20
Donald Suhr <git@donsuhr.com>
2022-05-30 12:36:20 -0700

    add import stub. move base case test first

--- src/leetcode/merge-two-sorted-lists-21.test.ts
+++ src/leetcode/merge-two-sorted-lists-21.test.ts
@@ -1,30 +1,43 @@
 import { mergeSorted, mergeSortedRecursive } from './merge-two-sorted-lists-21';
+// import {
+    // mergeSortedDaily,
+    // mergeSortedRecursiveDaily,
+// } from '../_daily/merge-sorted-list';
 import { LinkedList, arrayToLinkedList } from '../_data-structure/linked-list';
 
 describe.each([
     { fn: mergeSorted },
-    { fn: mergeSortedRecursive }
+    { fn: mergeSortedRecursive },
+    // { fn: mergeSortedDaily },
+    // { fn: mergeSortedRecursiveDaily },
 ])('merge sorted list: $fn.name', ({ fn: sort }) => {
-    it('sorts some things', () => {
-        const A = arrayToLinkedList([1, 2, 3]);
-        const B = arrayToLinkedList([1, 3, 4]);
-        const sorted = sort(A.getFirst(), B.getFirst());
-        const expected = arrayToLinkedList([1, 1, 2, 3, 3, 4]);
-        expect(JSON.stringify(sorted)).toEqual(JSON.stringify(expected.getFirst()));
-    });
-    it('sorts some other things', () => {
-        const A = arrayToLinkedList([1, 3, 4]);
-        const B = arrayToLinkedList([1, 2, 3]);
-        const sorted = sort(A.getFirst(), B.getFirst());
-        const expected = arrayToLinkedList([1, 1, 2, 3, 3, 4]);
-        expect(JSON.stringify(sorted)).toEqual(JSON.stringify(expected.getFirst()));
-    });
-
     it('base case', () => {
         const A = new LinkedList<any>();
         const B = arrayToLinkedList([1]);
         const sorted = sort(A.getFirst(), B.getFirst());
         const expected = arrayToLinkedList([1]);
-        expect(JSON.stringify(sorted)).toEqual(JSON.stringify(expected.getFirst()));
+        expect(JSON.stringify(sorted)).toEqual(
+            JSON.stringify(expected.getFirst()),
+        );
+    });
+
+    it('sorts some things', () => {
+        const A = arrayToLinkedList([1, 2, 3]);
+        const B = arrayToLinkedList([1, 3, 4]);
+        const sorted = sort(A.getFirst(), B.getFirst());
+        const expected = arrayToLinkedList([1, 1, 2, 3, 3, 4]);
+        expect(JSON.stringify(sorted)).toEqual(
+            JSON.stringify(expected.getFirst()),
+        );
+    });
+
+    it('sorts some other things', () => {
+        const A = arrayToLinkedList([1, 3, 4]);
+        const B = arrayToLinkedList([1, 2, 3]);
+        const sorted = sort(A.getFirst(), B.getFirst());
+        const expected = arrayToLinkedList([1, 1, 2, 3, 3, 4]);
+        expect(JSON.stringify(sorted)).toEqual(
+            JSON.stringify(expected.getFirst()),
+        );
     });
 });
