7311a8e603b356e381f321b07089e48789a69a40
Donald Suhr <git@donsuhr.com>
2022-08-26 15:14:25 -0700

    add bubble sort optimization

--- src/_sort/bubble-sort.ts
+++ src/_sort/bubble-sort.ts
@@ -1,11 +1,16 @@
 /* eslint-disable no-param-reassign */
 // O(n^2)
 function bubbleSort<T>(A: T[]): T[] {
+    let noSwaps = true;
     for (let i = A.length - 1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
             if (A[j] > A[j + 1]) {
                 [A[j], A[j + 1]] = [A[j + 1], A[j]];
+                noSwaps = false;
             }
+        }
+        if (noSwaps) {
+            break;
         }
     }
     return A;
