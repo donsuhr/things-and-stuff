4dd409be97cadfae60edc9e3cc78eb9bbeff03d1
Donald Suhr <git@donsuhr.com>
2022-12-30 12:47:39 -0800

    lint

--- src/_daily/linked-list.ts
+++ src/_daily/linked-list.ts
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */ // @ts-ignore
 import { LinkedList as answer } from '../_data-structure/linked-list';
-import { LinkedListNode as Node } from '../_data-structure/linked-list-node';
+// import { LinkedListNode as Node } from '../_data-structure/linked-list-node';
 
 export class LinkedListDaily {
     // getFirst() {}
--- src/_daily/stack.ts
+++ src/_daily/stack.ts
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */ // @ts-ignore
 import { Stack as answer } from '../_data-structure/stack';
-import { LinkedListNode as Node } from '../_data-structure/linked-list-node';
+// import { LinkedListNode as Node } from '../_data-structure/linked-list-node';
 
 export class StackDaily {
     // isEmpty() {}
